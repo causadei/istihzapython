2913b67c52b9e75d6ef35512a8926cb174cb9d5c
efe <honoriscausadei1@gmail.com>
2016-12-30 01:16:44 +0200

    Update 007_dort_uygulama_programi.py
    Hesap makinası tamamlandı

--- 000.JustForFunPrograms/007_dort_uygulama_programi.py
+++ 000.JustForFunPrograms/007_dort_uygulama_programi.py
@@ -8,12 +8,50 @@
 
 
 #1. Program - Hesap makinası Let's begin :)
-metin == """
-(1) Toplama
-(2) Çıkarma
-(3) Çarpma
-(4) Bölme
-Seçiminizi yapınız
-Çıkmak için (q) ya basınız
+aciklama = """
+[1] Toplama
+[2] Çıkarma
+[3] Çarpma
+[4] Bölme
+Çıkmak için Q tuşuna basınız.
 """
-#devam edecek :)
+d ="Devam etmek için bir tuşa basınız"
+while True:
+    try:
+        print(aciklama)
+        sec = input("Yapmak İstediğiniz İşlemi Seçiniz: ")
+        if sec == "1":
+            sayi = int(input("Birinci Sayı: "))
+            sayi2 = int(input("İkinci Sayı: "))
+            sonuc = sayi + sayi2
+            print("{} + {} = {}".format(sayi, sayi2, sonuc))
+            input(d)
+        elif sec == "2":
+            sayi = int(input("Birinci Sayı: "))
+            sayi2 = int(input("İkinci Sayı: "))
+            sonuc = sayi - sayi2
+            print("{} - {} = {}".format(sayi, sayi2, sonuc))
+            input(d)
+        elif sec == "3":
+            sayi = int(input("Birinci Sayı: "))
+            sayi2 = int(input("İkinci Sayı: "))
+            sonuc = sayi * sayi2
+            print("{} * {} = {}".format(sayi, sayi2, sonuc))
+            input(d)
+        elif sec == "4":
+            try:
+                sayi = int(input("Birinci Sayı: "))
+                sayi2 = int(input("İkinci Sayı: "))
+                sonuc = sayi / sayi2
+                print("{} / {} = {}".format(sayi, sayi2, sonuc))
+                input(d)
+            except ZeroDivisionError:
+                print("0'a bölünmez")
+        elif sec == "Q":
+            break
+        elif sec == "q":
+            break
+        else:
+            print("Lütfen Belirtilen Seçeneklerden Birini Seçiniz")
+    except:
+        print("Lütfen Rakam Giriniz")

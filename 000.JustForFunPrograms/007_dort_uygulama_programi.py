022635b181a5d30fa479e0fbce8becca2392ac80
efe <honoriscausadei1@gmail.com>
2017-01-01 02:21:42 +0200

    Update 007_dort_uygulama_programi.py

--- 000.JustForFunPrograms/007_dort_uygulama_programi.py
+++ 000.JustForFunPrograms/007_dort_uygulama_programi.py
@@ -57,3 +57,16 @@ while True:
             print("Lütfen Belirtilen Seçeneklerden Birini Seçiniz")
     except:
         print("Lütfen Rakam Giriniz")
+#2.Program 
+ad = input("Adınızı Girin: ")
+soyad = input("Soyadınızı Girin: ")
+dogum_tarihi = input("Doğum Tarihinizi Girin: ")
+cinsiyet = input("Cinsiyetinizi Girin: ")
+
+with open("deneme.txt","a+") as bilgiler:
+    bilgiler.write("\nKullanıcı adı: {}".format(ad))
+    bilgiler.write("\nKullanıcı Soyadı: {}".format(soyad))
+    bilgiler.write("\nKullanıcı doğum tarihi: {}".format(dogum_tarihi))
+    bilgiler.write("\nKullanıcı Cinsiyeti: {}".format(cinsiyet))
+
+
